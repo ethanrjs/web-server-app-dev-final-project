574db507b4616553e9d3706d6dc1e6b3e5290cf8
goudyeaUC <156120163+goudyeaUC@users.noreply.github.com>
2025-10-30 23:11:33 -0400

    New Game class

---
+++ Models/Game.cs
@@ -0,0 +1,13 @@
+public class Game
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Platform { get; set; }
+    public int Year { get; set; }
+    public string Maker { get; set; }
+    
+    public Game()
+    { 
+        
+    }
+}
